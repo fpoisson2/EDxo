a1f3766984933ccd45e99ab9db13d96fcbf75c35
fpoisson2 <francis.poisson2@gmail.com>
2024-12-20 16:10:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3176,4 +3176,4 @@ def view_competence(competence_id):
 # --------------------- Exécuter l'Application ---------------------
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
