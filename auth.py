269d7f850ec50af55ddd115ec3379aa3ef7fd5f6
fpoisson2 <francis.poisson2@gmail.com>
2024-12-21 23:33:12 -0500

    starting to split routes into different files

---
+++ auth.py
@@ -0,0 +1,17 @@
+from flask_login import LoginManager
+from utils import get_db_connection
+from models import User
+
+# Configure Flask-Login
+login_manager = LoginManager()
+login_manager.login_message = "Veuillez vous connecter pour accéder à cette page."
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = get_db_connection()
+    user = conn.execute('SELECT * FROM User WHERE id = ?', (user_id,)).fetchone()
+    conn.close()
+    if user:
+        return User(user['id'], user['username'], user['password'], user['role'])
+    return None
