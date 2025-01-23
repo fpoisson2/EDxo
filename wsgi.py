4caf747a054ce8d1cf2c1e417482ec597a01bb02
fpoisson2 <francis.poisson2@gmail.com>
2025-01-23 13:52:49 -0500

    test

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 from app import app
 from utilitaires.scheduler_instance import start_scheduler, schedule_backup
 
+schedule_backup(app)
+start_scheduler()
+
 if __name__ == "__main__":
-    # Initialize scheduler before workers are forked
-    schedule_backup(app)
-    start_scheduler()
     app.run()
